0be9cc2e473f0d8dfbc394681f8626b70ee21da8
j3ksa <yogalj@DESKTOP-CLB1F7G.localdomain>
2021-03-30 13:55:21 +0200

    Title change

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Banach Group</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
